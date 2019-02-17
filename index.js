41dc44a507ac5062f9a58a254f2d76e371055d10
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2019-02-16 20:34:09 -0500

    Cleanup

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 const states = {
-	pending: 'Pending',
-	resolved: 'Resolved',
-	rejected: 'Rejected'
+    pending: 'Pending',
+    resolved: 'Resolved',
+    rejected: 'Rejected'
 };
 
 class Nancy {
-	constructor(executor) {
-		const tryCall = callback => Nancy.try(() => callback(this.value));
-		const laterCalls = [];
-		const callLater = getMember => callback => new Nancy(resolve => laterCalls.push(() => resolve(getMember()(callback))));
-		const members = {
-			[states.resolved]: {
-				state: states.resolved,
-				then: tryCall,
-				catch: _ => this
-			},
-			[states.rejected]: {
-				state: states.rejected,
-				then: _ => this,
-				catch: tryCall
-			},
-			[states.pending]: {
-				state: states.pending,
-				then: callLater(() => this.then),
-				catch: callLater(() => this.catch)
-			}
-		};
-		const changeState = state => Object.assign(this, members[state]);
-		const apply = (value, state) => {
-			this.value = value;
-			changeState(state);
-			for (const laterCall of laterCalls) {
-				laterCall();
-			}
-		};
+    constructor(executor) {
+        const tryCall = callback => Nancy.try(() => callback(this.value));
+        const laterCalls = [];
+        const callLater = getMember => callback => new Nancy(resolve => laterCalls.push(() => resolve(getMember()(callback))));
+        const members = {
+            [states.resolved]: {
+                state: states.resolved,
+                then: tryCall,
+                catch: _ => this
+            },
+            [states.rejected]: {
+                state: states.rejected,
+                then: _ => this,
+                catch: tryCall
+            },
+            [states.pending]: {
+                state: states.pending,
+                then: callLater(() => this.then),
+                catch: callLater(() => this.catch)
+            }
+        };
+        const changeState = state => Object.assign(this, members[state]);
+        const apply = (value, state) => {
+            this.value = value;
+            changeState(state);
+            for (const laterCall of laterCalls) {
+                laterCall();
+            }
+        };
 
-		const getCallback = state => value => {
-			if (this.state === states.pending) {
-				if (value instanceof Nancy && state === states.resolved) {
-					value.then(value => apply(value, states.resolved));
-					value.catch(value => apply(value, states.rejected));
-				} else {
-					apply(value, state);
-				}
-			}
-		};
+        const getCallback = state => value => {
+            if (this.state === states.pending) {
+                if (value instanceof Nancy && state === states.resolved) {
+                    value.then(value => apply(value, states.resolved));
+                    value.catch(value => apply(value, states.rejected));
+                } else {
+                    apply(value, state);
+                }
+            }
+        };
 
-		const resolve = getCallback(states.resolved);
-		const reject = getCallback(states.rejected);
-		changeState(states.pending);
-		try {
-			executor(resolve, reject);
-		} catch (error) {
-			reject(error);
-		}
-	}
+        const resolve = getCallback(states.resolved);
+        const reject = getCallback(states.rejected);
+        changeState(states.pending);
+        try {
+            executor(resolve, reject);
+        } catch (error) {
+            reject(error);
+        }
+    }
 
-	static resolve(value) {
-		return new Nancy(resolve => resolve(value));
-	}
+    static resolve(value) {
+        return new Nancy(resolve => resolve(value));
+    }
 
-	static reject(value) {
-		return new Nancy((_, reject) => reject(value));
-	}
+    static reject(value) {
+        return new Nancy((_, reject) => reject(value));
+    }
 
-	static try(callback) {
-		return new Nancy(resolve => resolve(callback()));
-	}
+    static try(callback) {
+        return new Nancy(resolve => resolve(callback()));
+    }
 }
 
 module.exports = {Nancy, states};
--- test.js
+++ test.js
@@ -3,123 +3,121 @@ import {asyncCounter} from 'async-counter';
 import {Nancy, states} from '.';
 
 test('empty executor results in a pending promise', t => {
-	const p = new Nancy(() => {});
-	t.is(p.state, states.pending);
+    const p = new Nancy(() => {});
+    t.is(p.state, states.pending);
 });
 
 const throwSomethingWrong = () => {
-	throw new Error('Something went wrong...');
+    throw new Error('Something went wrong...');
 };
 
 test('error thrown during resolve execution results in a rejected promise', t => {
-	const p = new Nancy(throwSomethingWrong);
-	t.is(p.state, states.rejected);
+    const p = new Nancy(throwSomethingWrong);
+    t.is(p.state, states.rejected);
 });
 
 test('simple resolve and reject works', t => {
-	let p = Nancy.resolve(42);
-	t.is(p.state, states.resolved);
-	t.is(p.value, 42);
-	p = Nancy.reject(42);
-	t.is(p.state, states.rejected);
-	t.is(p.value, 42);
+    let p = Nancy.resolve(42);
+    t.is(p.state, states.resolved);
+    t.is(p.value, 42);
+    p = Nancy.reject(42);
+    t.is(p.state, states.rejected);
+    t.is(p.value, 42);
 });
 
 test('chain then sync', t => {
-	let p = Nancy.reject(42)
-		.then(() => 0)
-		.then(() => 1)
-		.then(() => 2);
-	t.is(p.state, states.rejected);
-	t.is(p.value, 42);
+    let p = Nancy.reject(42)
+        .then(() => 0)
+        .then(() => 1)
+        .then(() => 2);
+    t.is(p.state, states.rejected);
+    t.is(p.value, 42);
 
-	p = Nancy.resolve(0)
-		.then(value => {
-			t.is(value, 0);
-			return 1;
-		})
-		.then(value => {
-			t.is(value, 1);
-			return 2;
-		})
-		.then(throwSomethingWrong);
-	t.is(p.state, states.rejected);
+    p = Nancy.resolve(0)
+        .then(value => {
+            t.is(value, 0);
+            return 1;
+        })
+        .then(value => {
+            t.is(value, 1);
+            return 2;
+        })
+        .then(throwSomethingWrong);
+    t.is(p.state, states.rejected);
 });
 
-const anything = () => {
-	throw new Error('I can be anything because I should never get called!');
-};
+const anything = t => () => t.fail('anything should never be called.');
 
 test.cb('chain catch sync', t => {
-	Nancy.reject(42)
-		.catch(error => error)
-		.catch(anything)
-		.then(throwSomethingWrong)
-		.catch(throwSomethingWrong)
-		.catch(() => t.end());
+    Nancy.reject(42)
+        .catch(error => error)
+        .catch(anything(t))
+        .then(throwSomethingWrong)
+        .catch(throwSomethingWrong)
+        .catch(() => t.end());
 });
 
 test('subsequent resolves and rejects are ignored', t => {
-	const p = new Nancy((resolve, reject) => {
-		reject(42);
-		resolve(24);
-		reject();
-	});
-	t.is(p.state, states.rejected);
-	t.is(p.value, 42);
+    const p = new Nancy((resolve, reject) => {
+        reject(42);
+        resolve(24);
+        reject();
+    });
+    t.is(p.state, states.rejected);
+    t.is(p.value, 42);
 });
 
 test.cb('unpack promise value on resolve, not reject', t => {
-	Nancy.reject(Nancy.resolve(Nancy.reject(42)))
-		.catch(value => {
-			t.is(value instanceof Nancy, true);
-			return value;
-		})
-		.catch(value => {
-			t.is(value, 42);
-			return value;
-		})
-		.then(value => t.is(value, 42))
-		.then(() => Nancy.reject(24))
-		.catch(value => t.is(value, 24))
-		.then(() => t.end());
+    Nancy.reject(Nancy.resolve(Nancy.reject(42)))
+        .catch(value => {
+            t.is(value instanceof Nancy, true);
+            return value;
+        })
+        .catch(value => {
+            t.is(value, 42);
+            return value;
+        })
+        .then(value => t.is(value, 42))
+        .then(() => Nancy.reject(24))
+        .catch(value => t.is(value, 24))
+        .then(() => t.end());
 });
 
 const delay = () => new Nancy(resolve => setTimeout(resolve, 500));
 
 test.cb('chain then async', t => {
-	delay()
-		.then(delay)
-		.then(delay)
-		.then(() => t.end());
+    delay()
+        .then(delay)
+        .then(delay)
+        .then(() => t.end());
 });
 
 test.cb('chain catch async', t => {
-	delay()
-		.then(() => Nancy.reject())
-		.catch(throwSomethingWrong)
-		.catch(() => 42)
-		.catch(anything)
-		.then(value => t.is(value, 42))
-		.then(delay)
-		.then(throwSomethingWrong)
-		.catch(() => t.end());
+    delay()
+        .then(() => Nancy.reject())
+        .catch(throwSomethingWrong)
+        .catch(() => 42)
+        .catch(anything(t))
+        .then(value => t.is(value, 42))
+        .then(delay)
+        .then(throwSomethingWrong)
+        .catch(() => t.end());
 });
 
 test.cb('multiple then on single promise', t => {
-	const counter = asyncCounter(3, {onFinished: () => t.end()});
-	const p = delay();
-	p.then(counter.count);
-	p.then(counter.count);
-	p.then(delay).then(counter.count);
+    const counter = asyncCounter(3, {onFinished: () => t.end()});
+    const p = delay();
+    p.then(counter.count);
+    p.then(counter.count);
+    p.then(delay).then(counter.count);
 });
 
 test.cb('multiple catch on single promise', t => {
-	const counter = asyncCounter(3, {onFinished: () => t.end()});
-	const p = delay()
-		.then(() => Nancy.reject());
-	p.then(anything);
-	p.catch(counter.count);
-	p.catch(counter.count);
-	p.catch(delay).then(counter.count);
+    const counter = asyncCounter(3, {onFinished: () => t.end()});
+    const p = delay()
+        .then(() => Nancy.reject());
+    p.then(anything(t));
+    p.catch(counter.count);
+    p.catch(counter.count);
+    p.catch(delay).then(counter.count);
 });
